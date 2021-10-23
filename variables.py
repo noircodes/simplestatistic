0261c9d299685f8d74bee000a2256dc829055557
noircodes <ilhamizdhihar2007@gmail.com>
2021-10-23 08:50:19 +0700

    Correcting the formula
    Correcting the median and sigma.

--- variables.py
+++ variables.py
@@ -5,8 +5,8 @@ class variables :
     x = [26, 12, 23, 17, 22, 16, 12, 16, 18, 18, 18, 21, 25, 16, 15, 13, 16, 19]
     np_x = np.array(x)
     mean = np.mean(np_x)
-    median = np.round(np.median(np_x))
+    median = np.median(np_x)
     n = len(np_x)
-    sigma = np.sum(((np_x - median) ** 2))
+    sigma = np.sum(((np_x - mean) ** 2))
     vs = sigma / (n - 1)
 
